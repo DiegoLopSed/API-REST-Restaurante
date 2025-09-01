c923b5cdfe6fe80d8d1765b40ec27988f5ff066d
Diego Lopez Sedeño <diegolopsed160703@gmail.com>
2025-09-01 01:08:54 -0600

    Archivo de acceso acceso a la BD

---
+++ src/db.js
@@ -0,0 +1,15 @@
+import {createPool} from "mysql2/promise.js"
+import {DB_DATABASE,
+DB_HOST,
+DB_USER,
+DB_PASSWORD,
+DB_PORT
+}from "./config.js"
+
+export const connection = createPool({
+    host:DB_HOST,
+    user:DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database:DB_DATABASE
+})
